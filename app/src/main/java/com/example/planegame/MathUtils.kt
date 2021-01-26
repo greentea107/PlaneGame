6fca5ffc0d95187aebed3c0644d5e29713ed35eb
greentea107 <52137124@qq.com>
2021-01-26 11:13:56 +0800

    优化十字摇杆

--- app/src/main/java/com/example/planegame/MathUtils.kt
+++ app/src/main/java/com/example/planegame/MathUtils.kt
@@ -1,7 +1,9 @@
 package com.example.planegame
 
-import android.graphics.Point
+import android.graphics.PointF
 import android.graphics.Rect
+import android.util.LruCache
+import androidx.annotation.NonNull
 import kotlin.math.*
 
 object MathUtils {
@@ -17,23 +19,62 @@ object MathUtils {
     }
 
     /**
-     * 根据两个坐标获取角度
+     * 对COS和SIN的结果进行缓存，如果某个角度已经计算过则直接取缓存，不用每次都计算
+     * 缓存的KEY为保留两位小数的角度值，所以从缓存取值有精度不准的问题
      */
-    fun getAngle(p1: Point, p2: Point): Double {
-        val tem = atan(((p2.y - p1.y) / (p2.x - p1.x)).toDouble())
-        return tem * (180 / Math.PI)
+    private val cosCach = LruCache<String, Double>(360)
+    private val sinCache = LruCache<String, Double>(360)
+
+    /**
+     * @param isCache 是否使用缓存
+     */
+    private fun getCosByRadians(@NonNull angle: Double, isCache: Boolean = true): Double {
+        //将Double类型的角度值保留两位小数后转String类型，用于缓存的KEY值
+        val keyAngle = String.format("%.2f", angle)
+        return if (isCache) {
+            // 有缓存取缓存，没缓存就计算后再缓存
+            if (cosCach[keyAngle] != null)
+                cosCach[keyAngle]
+            else {
+                val result = cos(Math.toRadians(angle))
+                cosCach.put(keyAngle, result)
+                result
+            }
+        } else {
+            // 不用缓存直接计算
+            cos(Math.toRadians(angle))
+        }
     }
 
     /**
-     * 坐标按角度旋转
+     * @param isCache 是否使用缓存
      */
-    fun coordinateRotation(x: Float, y: Float, degree: Int): Point {
-        var x1 = x * cos(degree * Math.PI / 180) + y * sin(degree * Math.PI / 180);
-        x1 = ((x1 * 100).roundToInt() / 100).toDouble()
-        var y1 =
-            -x * sin(degree * Math.PI / 180) + y * cos(degree * Math.PI / 180);
-        y1 = ((y1 * 100).roundToInt() / 100).toDouble()
-        val relativeOriginPoint = Point(x1.toInt(), y1.toInt())
-        return relativeOriginPoint
+    private fun getSinByRadians(@NonNull angle: Double, isCache: Boolean = true): Double {
+        //将Double类型的角度值保留两位小数后转String类型，用于缓存的KEY值
+        val keyAngle = String.format("%.2f", angle)
+        return if (isCache) {
+            // 有缓存取缓存，没缓存就计算后再缓存
+            if (sinCache[keyAngle] != null)
+                sinCache[keyAngle]
+            else {
+                val result = sin(Math.toRadians(angle))
+                sinCache.put(keyAngle, result)
+                result
+            }
+        } else {
+            // 不用缓存直接计算
+            sin(Math.toRadians(angle))
+        }
+    }
+
+    /**
+     * 根据半径、角度计算对应的坐标
+     * 角度以三点钟方向为0度，顺时针方向增加
+     * @param ptOrgini 原点坐标，默认为0,0
+     */
+    fun getCoordsByAngle(radius: Float, angle: Double, ptOrgini: PointF = PointF()): PointF {
+        val x = ptOrgini.x + radius * getCosByRadians(angle)
+        val y = ptOrgini.y + radius * getSinByRadians(angle)
+        return PointF(x.toFloat(), y.toFloat())
     }
 }
